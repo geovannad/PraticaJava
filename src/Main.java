c5ade8a1107b7f5cc8998266adf5c8132755e2a5
geovannad <geovanna.diniz@germinare.org.br>
2024-06-23 16:27:43 -0300

    push dev

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner entrada = new Scanner(System.in);
         System.out.println("OIEEEEEE MUNDO !!!!!");
         System.out.println("Segundo teste");
+        System.out.println("Terceito teste");
 
 
         /*
